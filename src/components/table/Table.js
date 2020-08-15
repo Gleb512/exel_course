eb016d4071e5d9a8f90b40c7192bc33ca6c7787b
Gleb512 <glebordinskijj@icloud.com>
2020-08-16 01:54:40 +0300

    Resize finished

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,9 +1,22 @@
 import {ExelComponent} from '../../core/ExelComponent';
 import {createTable} from './table-template';
+import {resizeHandler} from '@/components/table/table.resize';
 
 export class Table extends ExelComponent {
     static className = 'exel__table'
+    constructor($root, options) {
+        super($root, {
+            name: 'Table',
+            listeners: ['mousedown']
+        });
+    }
     toHTML() {
         return createTable(20)
     }
+    onMousedown(event) {
+        if (event.target.dataset.resize) {
+            resizeHandler(this.$root, event)
+        }
+    }
 }
+
